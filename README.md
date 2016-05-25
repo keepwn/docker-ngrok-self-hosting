f72e81039f0ef534945c3c70cd3d674eeb31b23a
keepwn <keepwn@gmail.com>
2016-05-25 15:08:08 +0800

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # docker-ngrok-self-hosting
+
+## Build Image
+```
+docker build -t my/ngrok-self-hosting .
+```
+
+## Generate Ngrok-Self
+```
+cd ~
+mkdir ngrok-bin
+docker run --rm -e DOMAIN="tunnel.mydomain.com" -v ~/ngrok-bin:/release my/ngrok-self-hosting
+```
+Ngrok server and client binaries will be available in `~/ngrok-bin` on the host.
+
+## Environment Variables
+| variables   |  default   | meaning                                  |
+| ----------- | :--------: | ---------------------------------------- |
+| DOMAIN      | *required* | domain name that ngrok running on        |
+| TUNNEL_PORT |   `4443`   | port that ngrok server's control channel listens |
+| HTTP_PORT   |    `80`    | port that ngrok server's http tunnel listents |
+| HTTPS_PORT  |   `443`    | port that ngrok server's https tunnel listents |
+
+## Run Server
+You need copy `ngrok-bin` to server, and `docker` must be installed in server.
+```
+cd ngrok-bin
+./run_server.sh
+```
---
+++ build.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+if [ "${DOMAIN}" == "**None**"  ]; then
+    echo "Please Input DOMAIN"
+    exit 1
+fi
+
+echo "=> 1. Clone Latest Ngrok ..."
+cd /
+git clone https://github.com/inconshreveable/ngrok.git
+
+echo "=> 2. Creating Tls ..."
+NGROK_DOMAIN=${DOMAIN}
+mkdir /ngrok/tls && cd /ngrok/tls
+openssl genrsa -out rootCA.key 2048
+openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$NGROK_DOMAIN" -days 5000 -out rootCA.pem
+openssl genrsa -out device.key 2048
+openssl req -new -key device.key -subj "/CN=$NGROK_DOMAIN" -out device.csr
+openssl x509 -req -in device.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out device.crt -days 5000
+
+cp rootCA.pem /ngrok/assets/client/tls/ngrokroot.crt
+
+echo "=> 3. Building Ngrok ..."
+cd /ngrok
+make release-server release-client
+echo "=> Build Successfully"
+
+echo "=> 4. Outputing Bin To /release"
+cp /ngrok/bin/* /release/
+cp /ngrok/tls/* /release/
+
+echo "=> 5. Generating Config Files ..."
+cat > /release/ngrok.conf <<EOF
+server_addr: ${DOMAIN}:${TUNNEL_PORT}
+trust_host_root_certs: false
+EOF
+
+cat > /release/run_server.sh <<EOF
+#!/bin/sh
+DOMAIN=${DOMAIN}
+TUNNEL_PORT=${TUNNEL_PORT}
+HTTP_PORT=${HTTP_PORT}
+HTTPS_PORT=${HTTPS_PORT}
+
+docker run --name=ngrok-server \
+           --restart=always -d \
+           -v \$(pwd):/release \
+           -p $TUNNEL_PORT:4443 \
+           -p $HTTP_PORT:80 \
+           -p $HTTPS_PORT:443 \
+           alpine \
+           ./release/ngrokd -tlsKey=/release/device.key -tlsCrt=/release/device.crt -domain="$DOMAIN" -httpAddr=":80" -httpsAddr=":443" -tunnelAddr=":4443"
+EOF
+
+echo "=> Finish."
